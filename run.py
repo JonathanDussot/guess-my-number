d6337a4a6d4a31eb40ed2496a6ec9bf6e5c99984
JonathanDussot <jonathandussot@gmail.com>
2024-02-07 00:28:41 +0000

    Code reverted and fully functional again

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-from colorama import init, Fore
 
 
 def user_name():
@@ -15,10 +14,10 @@ def user_name():
         try:
             name = input('Enter your name here: ')
             if not name.strip():
-                raise ValueError(Fore.RED + 'Not a valid name')
+                raise ValueError('Not a valid name')
             elif not name.isalpha():
-                raise ValueError(Fore.RED + 'Invalid name. Please enter '
-                                 'alphabetic characters only.')
+                raise ValueError('Invalid name. Please enter alphabetic'
+                                 ' characters only.')
             break
         except ValueError as e:
             print(e)
@@ -43,8 +42,8 @@ def choose_level(name):
             print(f'\nGreat {name}, you have chosen {level}\n')
             break
         else:
-            print(Fore.RED + f'Sorry {name}, you have entered an invalid '
-                  'option. Please choose a valid game difficulty\n')
+            print(f'Sorry {name}, you have entered an invalid option. Please'
+                  ' choose a valid game difficulty\n')
 
     return level
 
@@ -72,7 +71,7 @@ def generate_random_value(name, chosen_level):
         print(f"I\'m thinking of a number, {name}! You have 13 attempts to "
               "guess which number it is.")
     else:
-        print(Fore.RED + 'Invalid game level')
+        print('Invalid game level')
 
     return random_number, attempts
 
@@ -86,18 +85,18 @@ def approximity(guess, random_number, guessed_numbers, difference_list):
     difference_list.append(difference)
 
     if 1 <= difference < 10:
-        print(Fore.YELLOW + 'You\'re so close now!')
+        print('You\'re so close now!')
 
     if len(difference_list) >= 2:
         last_difference = difference_list[-1]
         penultimate_difference = difference_list[-2]
 
         if difference == 0:
-            print(Fore.GREEN + 'Spot on!')
+            print('Spot on!')
         elif last_difference < penultimate_difference:
-            print('You\'re getting', Fore.RED + 'warmer!')
+            print('You\'re getting warmer!')
         elif last_difference > penultimate_difference:
-            print('You\'re getting', Fore.BLUE + 'colder!')
+            print('Colder!')
 
 
 def check_answer(name, random_number_and_attempts, chosen_level,
@@ -116,15 +115,14 @@ def check_answer(name, random_number_and_attempts, chosen_level,
         if guess.isdigit():
             guess = int(guess)
         else:
-            print(Fore.RED + 'Invalid input. Please enter a number.')
+            print('Invalid input. Please enter a number.')
             continue
 
         difference = approximity(guess, random_number, guessed_numbers,
                                  difference_list)
 
         if guess in guessed_numbers:
-            print(Fore.RED + 'You already guessed that number. Try a '
-                  'different one.')
+            print('You already guessed that number. Try a different one.')
             continue
 
         if 1 <= guess <= get_max_value(chosen_level):
@@ -135,32 +133,30 @@ def check_answer(name, random_number_and_attempts, chosen_level,
                 attempts -= 1
 
                 if attempts == 3:
-                    print(Fore.BLUE + 'You have 3 attempts left, so here are '
-                          'some clues to help!')
+                    print('You have 3 attempts left, so here are some clues '
+                          'to help!')
                     if random_number % 3 == 0 and random_number % 4 == 0:
-                        print(Fore.BLUE + 'This number is divisible by 3 and'
-                              ' 4\n')
+                        print('This number is divisible by 3 and 4\n')
                     elif random_number % 3 == 0 and random_number % 4 != 0:
-                        print(Fore.BLUE + 'This number is divisible by 3 but '
-                              'not divisible by 4\n')
+                        print('This number is divisible by 3 but not divisible'
+                              ' by 4\n')
                     elif random_number % 3 != 0 and random_number % 4 == 0:
-                        print(Fore.BLUE + 'This number is divisible by 4 but '
-                              'not divisible by 3\n')
+                        print('This number is divisible by 4 but not divisible'
+                              ' by 3\n')
                     elif random_number % 3 != 0 and random_number % 4 != 0:
-                        print(Fore.BLUE + 'This number is not divisible by '
-                              'neither 3 nor 4\n')
+                        print('This number is not divisible by neither 3 nor '
+                              '4\n')
                 elif attempts == 0:
-                    print(Fore.RED + f'Sorry {name}, you have run out of '
-                          f'attempts. Game over.  The correct answer was '
-                          f'actually {random_number}')
+                    print(f'Sorry {name}, you have run out of attempts. Game '
+                          f'over.  The correct answer was actually '
+                          f'{random_number}')
                     break
             else:
-                print(Fore.GREEN + f'Hooray, {name}! You guessed the correct '
-                      'number!')
+                print(f'Hooray, {name}! You guessed the correct number!')
                 break
         else:
-            print(Fore.RED + f'Invalid guess. Please enter a number between 1 '
-                  f'and {get_max_value(chosen_level)}.')
+            print(f'Invalid guess. Please enter a number between 1 and '
+                  f'{get_max_value(chosen_level)}.')
 
     return random_number, attempts
 
@@ -200,13 +196,13 @@ def main():
         while True:
             play_again = input('Do you want to play again? (yes/no): ')
             if play_again.lower() == 'yes':
-                print(Fore.YELLOW + "That\'s the spirit")
+                print("That\'s the spirit")
                 break
             elif play_again.lower() == 'no':
-                print(Fore.YELLOW + "Thanks for playing. Goodbye!")
+                print("Thanks for playing. Goodbye!")
                 return
             else:
-                print(Fore.RED + "please only write \'yes\' or \'no\'")
+                print("please only write \'yes\' or \'no\'")
 
 
 main()
